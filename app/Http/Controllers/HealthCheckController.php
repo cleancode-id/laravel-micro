5405eeebd394633444807e8d5fdf5774aa4f1dc3
Yoga Hanggara <yohang88@gmail.com>
2020-11-09 00:19:18 +0700

    handling database empty on health check
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- app/Http/Controllers/HealthCheckController.php
+++ app/Http/Controllers/HealthCheckController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HealthCheckController extends Controller
 {
@@ -14,6 +15,13 @@ class HealthCheckController extends Controller
      */
     public function __invoke(Request $request)
     {
+        // try to connect DB health check
+        $dbConnection = config('database.default');
+
+        if (!empty($dbConnection)) {
+            DB::connection()->getPdo();
+        }
+
         return [
             'app'    => config('app.name'),
             'server' => gethostname(),
