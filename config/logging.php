285081bc4412ed6ffe61f45070714699b2396118
Yoga Hanggara <yohang88@gmail.com>
2021-04-25 19:11:47 +0700

    logging add default log level

--- config/logging.php
+++ config/logging.php
@@ -74,6 +74,7 @@ return [
 
         'stdout' => [
             'driver' => 'monolog',
+            'level' => env('LOG_LEVEL', 'debug'),
             'handler' => StreamHandler::class,
             'with' => [
                 'stream' => 'php://stdout',
@@ -82,6 +83,7 @@ return [
 
         'stderr' => [
             'driver' => 'monolog',
+            'level' => env('LOG_LEVEL', 'debug'),
             'handler' => StreamHandler::class,
             'formatter' => env('LOG_STDERR_FORMATTER'),
             'with' => [
